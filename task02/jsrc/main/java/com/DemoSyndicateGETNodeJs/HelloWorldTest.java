97a3725db63e2e93d37952eb8e038efdeb9c6c2a
yevhentereshchenko <Yevhen_Tereshchenko@epam.com>
2024-04-18 12:41:36 +0300

    fix task02

--- task02/jsrc/main/java/com/DemoSyndicateGETNodeJs/HelloWorldTest.java
+++ task02/jsrc/main/java/com/DemoSyndicateGETNodeJs/HelloWorldTest.java
@@ -2,29 +2,54 @@ package com.DemoSyndicateGETNodeJs;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
-@LambdaUrlConfig(authType = AuthType.NONE, invokeMode = InvokeMode.BUFFERED)
-@LambdaHandler(lambdaName = "hello_world-test",
-	roleName = "hello_world-test-role",
-	isPublishVersion = true,
-	//aliasName = "${lambdas_alias_name}",
-	aliasName = "hello",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-public class HelloWorldTest implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+@LambdaHandler(
+		lambdaName = "hello_world-test",
+		roleName = "hello_world-test-role",
+		isPublishVersion = true,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorldTest implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+	private final Gson gson = new Gson();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+		Map<String, String> queryStringParameters = apiGatewayProxyRequestEvent.getQueryStringParameters();
+		try {
+			String responseBody = gson.toJson("Hello from Lambda");
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_OK)
+					.withBody(responseBody);
+		} catch (Exception exception) {
+			String responseBody = gson.toJson("Error");
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_BAD_REQUEST)
+					.withBody(responseBody);
+		}
 	}
+
+
+
 }
+
